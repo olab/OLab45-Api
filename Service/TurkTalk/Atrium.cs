bdd6ada037c270eb2413177ef8dfd31decad4a8d
Corey <reg@pathrider.ca>
2022-08-04 13:00:53 -0600

    Cleanup

--- Service/TurkTalk/Atrium.cs
+++ Service/TurkTalk/Atrium.cs
@@ -3,9 +3,6 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text.Json;
-using OLabWebAPI.Services;
-using Microsoft.AspNetCore.SignalR;
 
 namespace OLabWebAPI.Services
 {
