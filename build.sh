4d0944a1dffccdad195d67cddde44e632357f0be
Corey Wirun <reg@pathrider.ca>
2022-12-11 16:33:07 +0000

    Updated support files

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-service olab45api stop
-dotnet clean
-dotnet build -c Release
-service olab45api start
-service olab45api status
+service olab46api stop
+dotnet clean OLab4WebApi.sln
+dotnet build -c Release OLab4WebApi.sln
+service olab46api start
+service olab46api status
