484cb8296005b1532674b3eb04ab05490d6527cb
Corey <reg@pathrider.ca>
2024-01-07 12:46:32 -0700

    TTalk updates

--- FunctionApp/Functions/TurkTalk/AssignLearnerFunction.cs
+++ FunctionApp/Functions/TurkTalk/AssignLearnerFunction.cs
@@ -12,27 +12,27 @@ namespace OLab.FunctionApp.Functions.SignalR
 {
   public partial class TurkTalkFunction : OLabFunction
   {
-    [Function("AssignLearner")]
-    [SignalROutput(HubName = "Hub")]
-    public async Task<IList<object>> AssignLearner(
-      [SignalRTrigger("Hub", "messages", "AssignLearner", "payload")] SignalRInvocationContext invocationContext,
-      AssignLearnerRequest payload)
-    {
-      payload.ConnectionId = invocationContext.ConnectionId;
-      // decrypt the user token from the payload
-      payload.DecryptAndRefreshUserToken(_configuration.GetAppSettings().Secret);
+    //[Function("AssignLearner")]
+    //[SignalROutput(HubName = "Hub")]
+    //public async Task<IList<object>> AssignLearner(
+    //  [SignalRTrigger("Hub", "messages", "AssignLearner", "payload")] SignalRInvocationContext invocationContext,
+    //  AssignLearnerRequest payload)
+    //{
+    //  payload.ConnectionId = invocationContext.ConnectionId;
+    //  // decrypt the user token from the payload
+    //  payload.DecryptAndRefreshUserToken(_configuration.GetAppSettings().Secret);
 
-      var endpoint = new TurkTalkEndpoint(
-        Logger,
-        _configuration,
-        DbContext,
-        TtalkDbContext,
-        _conference);
+    //  var endpoint = new TurkTalkEndpoint(
+    //    Logger,
+    //    _configuration,
+    //    DbContext,
+    //    TtalkDbContext,
+    //    _conference);
 
-      await endpoint.AssignLearnerAsync(payload);
+    //  //await endpoint.AssignLearnerAsync(payload);
 
-      Logger.LogInformation(JsonSerializer.Serialize(endpoint.MessageQueue.Messages));
-      return endpoint.MessageQueue.Messages;
-    }
+    //  Logger.LogInformation(JsonSerializer.Serialize(endpoint.MessageQueue.Messages));
+    //  return endpoint.MessageQueue.Messages;
+    //}
   }
 }
