594a9ee62b380633b42d6c165155093d91c28631
Corey <reg@pathrider.ca>
2024-01-09 15:18:12 -0700

    TTalk updates

--- FunctionApp/Functions/TurkTalk/RegisterLearnerFunction.cs
+++ FunctionApp/Functions/TurkTalk/RegisterLearnerFunction.cs
@@ -11,27 +11,27 @@ namespace OLab.FunctionApp.Functions.SignalR
 {
   public partial class TurkTalkFunction : OLabFunction
   {
-    //[Function("RegisterLearner")]
-    //[SignalROutput(HubName = "Hub")]
-    //public async Task<IList<object>> RegisterLearner(
-    //  [SignalRTrigger("Hub", "messages", "RegisterLearner", "payload")] SignalRInvocationContext invocationContext,
-    //  RegisterParticipantRequest payload)
-    //{
-    //  payload.ConnectionId = invocationContext.ConnectionId;
-    //  // decrypt the user token from the payload
-    //  payload.DecryptAndRefreshUserToken(_configuration.GetAppSettings().Secret);
+    [Function("RegisterLearner")]
+    [SignalROutput(HubName = "Hub")]
+    public async Task<IList<object>> RegisterLearner(
+      [SignalRTrigger("Hub", "messages", "RegisterLearner", "payload")] SignalRInvocationContext invocationContext,
+      RegisterParticipantRequest payload)
+    {
+      payload.ConnectionId = invocationContext.ConnectionId;
+      // decrypt the user token from the payload
+      payload.DecryptAndRefreshUserToken(_configuration.GetAppSettings().Secret);
 
-    //  var endpoint = new TurkTalkEndpoint(
-    //    Logger,
-    //    _configuration,
-    //    DbContext,
-    //    TtalkDbContext,
-    //    _conference);
+      var endpoint = new TurkTalkEndpoint(
+        Logger,
+        _configuration,
+        DbContext,
+        TtalkDbContext,
+        _conference);
 
-    //  await endpoint.RegisterLearnerAsync(payload);
+      await endpoint.RegisterLearnerAsync(payload);
 
-    //  Logger.LogInformation(JsonSerializer.Serialize(endpoint.MessageQueue.Messages));
-    //  return endpoint.MessageQueue.Messages;
-    //}
+      Logger.LogInformation(JsonSerializer.Serialize(endpoint.MessageQueue.Messages));
+      return endpoint.MessageQueue.Messages;
+    }
   }
 }
