42b311404008c0908df33481246c88b3f4b48412
Corey <reg@pathrider.ca>
2024-01-22 16:46:05 -0700

    ttalk updates

--- FunctionApp/Functions/TurkTalk/ConnectionFunctions.cs
+++ FunctionApp/Functions/TurkTalk/ConnectionFunctions.cs
@@ -1,3 +1,4 @@
+using Common.Utils;
 using Microsoft.Azure.Functions.Worker;
 using OLab.Access;
 using OLab.TurkTalk.Data.Contracts;
@@ -9,62 +10,6 @@ namespace OLab.FunctionApp.Functions.SignalR;
 
 public partial class TurkTalkFunction : OLabFunction
 {
-
-  [Function("OnConnected")]
-  [SignalROutput(HubName = "Hub")]
-  public SignalRMessageAction OnConnected(
-    [SignalRTrigger("Hub", "connections", "connected")] SignalRInvocationContext invocationContext)
-  {
-    //invocationContext.Headers.TryGetValue("Authorization", out var auth);
-    Logger.LogInformation($"{invocationContext.ConnectionId} has connected");
-
-    var endpoint = new TurkTalkEndpoint(
-      Logger,
-      _configuration,
-      _conference);
-
-    try
-    {
-
-      if (invocationContext.Query.TryGetValue("olab_access_token", out var accessToken))
-      {
-        var auth = new OLabAuthentication(Logger, _configuration, DbContext);
-
-        if (!invocationContext.Query.TryGetValue("sessionId", out var sessionId))
-          return endpoint.OnNotAuthenticated(
-            _configuration,
-            invocationContext.ConnectionId,
-            "Session Id was not provided");
-
-        auth.ValidateToken(accessToken);
-
-        var physParticipant = endpoint.GetParticipant(auth, sessionId);
-
-        return endpoint.OnAuthenticated(
-          _configuration,
-          invocationContext.ConnectionId,
-          physParticipant,
-          auth);
-
-      }
-
-      return endpoint.OnNotAuthenticated(
-        _configuration,
-        invocationContext.ConnectionId,
-        "Access token was not provided");
-
-    }
-    catch (System.Exception ex)
-    {
-      return endpoint.OnNotAuthenticated(
-        _configuration,
-        invocationContext.ConnectionId,
-        ex.Message);
-
-    }
-
-  }
-
 
   [Function("OnDisconnected")]
   [SignalROutput(HubName = "Hub")]
@@ -83,7 +28,6 @@ public partial class TurkTalkFunction : OLabFunction
 
     return await endpoint.OnDisconnectedAsync(
       _configuration,
-      invocationContext.ConnectionId,
       payload);
 
   }
