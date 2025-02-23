72a367de83de5fdaec5764ae9317eec31de4cbba
Corey Wirun <reg@pathrider.ca>
2025-02-22 20:28:51 -0700

    GroupRoleAcl tests

--- OLab.Azure/curl/login.sh
+++ OLab.Azure/curl/login.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -Ss -k --json "{\"Username\":\"$1\",\"Password\":\"$2\"}"  https://localhost:7071/olab/api/v3/auth/login > login.json
+curl -Ss -k --json "{\"Username\":\"$1\",\"Password\":\"$2\"}"  http://localhost:7071/olab/api/v3/auth/login > login.json
 cat login.json | jq
 export TOKEN="Bearer `cat login.json | jq '.data.authInfo.token'`"
 export TOKEN=`echo $TOKEN | tr -d \"`
---
+++ OLab.Test/ReaderWriters/GroupRoleAclReaderWriterTests.cs
@@ -0,0 +1,77 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Moq;
+using OLab.Api.Data.Exceptions;
+using OLab.Api.Model;
+using OLab.Api.Utils;
+using OLab.Common.Interfaces;
+using OLab.Data.Model;
+using OLab.Data.ReaderWriters;
+using OLab.Test.Utils;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace OLab.Test;
+
+public class GroupRoleAclReaderWriterTests
+{
+  private readonly IOLabLogger _logger;
+  private readonly Mock<OLabDBContext> _mockDbContext;
+  public readonly Mock<IOLabConfiguration> _mockConfiguration;
+
+  private readonly GroupRoleAclReaderWriter _groupRoleAclReaderWriter;
+  private readonly Mock<RoleReaderWriter> _mockRoleReaderWriter;
+
+  public GroupRoleAclReaderWriterTests()
+  {
+    _logger = new Mock<IOLabLogger>().Object;
+    _mockDbContext = new Mock<OLabDBContext>();
+    _mockConfiguration = new Mock<IOLabConfiguration>();
+
+    TestUtilities.MoqGroupRoleAclFromJsonFile( _mockDbContext, "json\\GroupRoleAcls.json" );
+
+    _mockRoleReaderWriter = new Mock<RoleReaderWriter>( _logger, _mockDbContext.Object );
+    _groupRoleAclReaderWriter = new GroupRoleAclReaderWriter( _logger, _mockDbContext.Object );
+  }
+
+  [Fact]
+  public async Task EditAsync_ValidAcl_ReturnsEditedAcl()
+  {
+    var acl = new GrouproleAcls { Id = 1, GroupId = 1, RoleId = 1, Acl2 = 7 };
+    _mockDbContext.Setup( db => db.GrouproleAcls.Update( acl ) );
+    _mockDbContext.Setup( db => db.SaveChangesAsync( It.IsAny<CancellationToken>() ) ).ReturnsAsync( 1 );
+
+    var result = await _groupRoleAclReaderWriter.EditAsync( acl, true );
+
+    Assert.Equal( acl, result );
+  }
+
+  [Fact]
+  public async Task GetByUserGroupRoles_ReturnsAcls()
+  {
+    var result = await _groupRoleAclReaderWriter.GetAsync();
+    Assert.Equal( 8, result.Count() );
+  }
+
+  [Fact]
+  public async Task GetListAsync_ReturnsAcls()
+  {
+    var result = await _groupRoleAclReaderWriter.GetListAsync( null, null, null, null );
+    Assert.Equal( 8, result.Count() );
+  }
+
+  [Fact]
+  public async Task GetListAsyncAllNodes_ReturnsNone()
+  {
+    var result = await _groupRoleAclReaderWriter.GetListAsync( null, null, Constants.ScopeLevelNode, null );
+    Assert.Empty( result );
+  }
+
+  [Fact]
+  public async Task GetListAsyncAllMaps_ReturnsAcls()
+  {
+    var result = await _groupRoleAclReaderWriter.GetListAsync( null, null, Constants.ScopeLevelMap, null );
+    Assert.Equal( 5, result.Count() );
+  }
+}
